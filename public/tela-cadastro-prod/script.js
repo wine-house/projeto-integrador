1a948ea3d0ffee38e63ba4070701453d532974d7
RodrigoPotel <rodrigopotel@gmail.com>
2022-11-05 19:12:13 -0300

    melhorando evento de adicao de foto

--- public/tela-cadastro-prod/script.js
+++ public/tela-cadastro-prod/script.js
@@ -5,9 +5,22 @@ foto.addEventListener('click', () => {
     inputFoto.click()
 })
 
+inputFoto.addEventListener('change', (e) => {
+    e.preventDefault();
+    console.log(e);
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        foto.src = reader.result;
+    }
+
+    reader.readAsDataURL(inputFoto.files[0]);
+})
+
 let enviarFormulario = document.getElementById('btn-submit');
 
 enviarFormulario.addEventListener('click', (e) => {
     e.preventDefault();
     alert('Seu formulário foi enviado!');
 });
+
