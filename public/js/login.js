7779984e0efdf2ac8fef61b2b1cd3065b9d3ea0f
Keyla Delpupo <ky.agro@gmail.com>
2022-11-06 23:02:28 -0300

    adiciona css parceiros

---
+++ public/js/login.js
@@ -0,0 +1,17 @@
+
+
+//window.onload = function () {
+ //   let fieldEmail = document.getElementById('e-mail');
+ //   let fieldPassword = document.getElementById('password');
+
+  //  fieldEmail.addEventListener('focus', function () {
+  //      console.log("funcionando campo");
+
+
+ //       fieldPassword.addEventListener('focus', function () {
+  //          console.log("funcionando campo");
+   //     });
+ //   });
+//
+
+
