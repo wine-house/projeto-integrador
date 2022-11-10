fca12fbc7c074e115ce71be41f2c4d3e2e7c7210
Keyla Delpupo <ky.agro@gmail.com>
2022-11-10 16:59:12 -0300

    adiciona validação login

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,46 @@
 
+let loginForm = document.querySelector('.login');
 
-//window.onload = function () {
- //   let fieldEmail = document.getElementById('e-mail');
- //   let fieldPassword = document.getElementById('password');
-
-  //  fieldEmail.addEventListener('focus', function () {
-  //      console.log("funcionando campo");
+loginForm.addEventListener('submit', function (event) {
 
 
- //       fieldPassword.addEventListener('focus', function () {
-  //          console.log("funcionando campo");
-   //     });
- //   });
-//
+    let fieldEmail = document.getElementById('email-login');
+    if (fieldEmail.value == '') {
+        event.preventDefault();
+        document.querySelector('.err-login-email').classList.remove('invisible')
+    } else {
+        document.querySelector('.err-login-email').classList.add('invisible')
+    }
+
+    let fieldSenha = document.getElementById('password');
+    if (fieldSenha.value.length < 6) {
+        event.preventDefault();
+        document.querySelector('.err-login-password').classList.remove('invisible')
+    } else {
+        document.querySelector('.err-login-password').classList.add('invisible')
+    }
+
+});
+
+let registerForm = document.querySelector('.form-cadastro');
+let registerInputs = document.querySelectorAll('.form-cadastro input')
+registerForm.addEventListener('submit', function (event) {
+
+    registerInputs.forEach(input => {
+
+        if (input.value == '') {
+            event.preventDefault();
+            document.querySelector('.err-register-' + input.name).classList.remove('invisible')
+        } else {
+            document.querySelector('.err-register-' + input.name).classList.add('invisible')
+        }
+    })
+
+
+})
+
+
+
+
 
 
