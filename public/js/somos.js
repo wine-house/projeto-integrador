c7391a4e7dce706ebbfbb80581da404f4a30dd81
ChristianBDevezas <christianbdevezas@hotmail.com>
2022-11-11 20:40:24 -0300

    atualiza página quem somos

--- public/js/somos.js
+++ public/js/somos.js
@@ -1,8 +1,9 @@
+//Banner2
 const banner2Container = document.querySelector(".banner2");
 const banner2Images = document.querySelectorAll(".banner2__image");
 const banner2Bullets = document.querySelectorAll(".banner2__bullets figure");
 const banner2Text = document.querySelector(".banner2__text");
-const arrayText = ['Trabalhamos em parceria com diversas vinícolas', 'Trabalhamos com vinhos Tinto, Seco, Rose e Espumante', 'Sorteio mensal para participar de eventos gratuitamente', 'Oferecemos diariamente descontos em nossos vinhos', 'Cadastre-se em nosso site e concorra a vários prêmios', 'Ganhe um brinde em sua primeira compra', 'A cada 10 compras, ganhe um kit de vinhos', 'Sempre disponibilizamos vinhos de ótima qualidade'];
+const arrayText = ['Trabalhamos em parceria com diversas vinícolas', 'Disponibilizamos vinhos Tinto, Seco, Rose e Espumante', 'Sorteio mensal para participar de eventos gratuitamente', 'Oferecemos diariamente descontos em nossos vinhos', 'Cadastre-se em nosso site e concorra a vários prêmios', 'Ganhe um brinde em sua primeira compra', 'A cada 10 compras, ganhe um kit de vinhos', 'Sempre disponibilizamos vinhos de ótima qualidade'];
 let counter = 0;
 
 banner2Text.innerText = arrayText[0];
@@ -47,3 +48,13 @@ function changeText() {
     if(counter > arrayText.length - 1) counter = 0;
     banner2Text.innerText = arrayText[counter];
 }
+
+//Destaques
+const contactMeBtn = document.querySelectorAll(".emphasis__bio__btn");
+
+contactMeBtn.forEach(button => {
+    button.addEventListener("click", (e) => {
+        const changeManager = e.currentTarget.parentElement.parentElement;
+        changeManager.classList.toggle("change-manager");
+    });
+});
