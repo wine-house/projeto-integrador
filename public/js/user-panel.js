269723285d0051baa7651d0ae976bee62e164bda
ChristianBDevezas <christianbdevezas@hotmail.com>
2022-11-21 14:59:40 -0300

    atualiza modal

--- public/js/user-panel.js
+++ public/js/user-panel.js
@@ -9,6 +9,7 @@ userBarInner.style.width = `${shoppingNumber * 10}%`;
 
 // Modal
 const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal__content");
 const userPurchase = document.getElementById("user-purchase");
 const modalCloseBtn = document.querySelector(".modal__content__close");
 
@@ -16,10 +17,12 @@ setTimeout(() => modal.classList.add("show-modal"), 2000);
 userPurchase.innerHTML = `${shoppingNumber}`;
 
 modalCloseBtn.addEventListener("click", () => {
-    modal.classList.remove("show-modal");
+    modalContent.classList.add("change-modal");
+    setTimeout(() => modal.classList.remove("show-modal"), 500);
 });
 
 modal.addEventListener("click", (e) => {
     if(e.target !== modal) return;
-    modal.classList.remove("show-modal");
+    modalContent.classList.add("change-modal");
+    setTimeout(() => modal.classList.remove("show-modal"), 500);
 });
