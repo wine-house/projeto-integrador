a7bf6334994fa5dfbfd2aa88ac0f8c4fb2a44f7a
computador-matheus <matheusfotografia987@gmail.com>
2023-02-23 15:46:51 -0300

    #/deixando-no-padrão-mvc

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,12 +1,37 @@
-const produtosModel = require('../models/produtosModel');
+const { Produto } = require('../../models');
 
 module.exports = {
-    index: (req, res) => {
+    index: async(req, res) => {
         // controller comunicando com o model
-        const produtos = produtosModel.index();
+        const produtos = await Produto.findAll();
         // controller comunicando com a view
         return res.render('index', {produtos: produtos,
             css: ['/stylesheets/home.css', '/stylesheets/menu-footer.css']
+        });
+    },
+    somos: (req, res, next) => {
+        res.render('somos', {
+          css: ['/stylesheets/somos.css', '/stylesheets/menu-footer.css']
+        });
+    },
+    eventos: (req, res, next) => {
+        res.render('eventos', {
+            css: ['/stylesheets/menu-footer.css', '/stylesheets/eventos.css']
+          });
+    },
+    parceiros: (req, res, next) => {
+        res.render('parceiros', {
+          css: ['/stylesheets/parceiros.css', '/stylesheets/menu-footer.css']
+        });
+    },
+    login: (req, res, next) => {
+        res.render('login', {
+          css: ['/stylesheets/menu-footer.css', '/stylesheets/login.css']
+        });
+    },
+    carrinho: (req, res) => {
+        res.render('carrinho', {
+          css: ['/stylesheets/menu-footer.css','/stylesheets/carrinho.css']
         });
     }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,71 +1,30 @@
-var express = require('express');
-const IndexController = require('../controller/indexController');
+const express = require('express');
+const { index, somos, eventos, parceiros, login, carrinho } = require('../controller/indexController');
 const produtos = require('../database/produtos.json');
 
-var router = express.Router();
+const router = express.Router();
 
-router.get('/', IndexController.index);
+router.get('/', index);
 
 /* pagina quem somos*/
-router.get('/somos', function(req, res, next) {
-  res.render('somos', {
-    css: ['/stylesheets/somos.css', '/stylesheets/menu-footer.css']
-  });
-});
+router.get('/somos', somos);
 
 /* pagina eventos*/
-router.get('/eventos', function(req, res, next) {
-  res.render('eventos',
-    {
-      css: ['/stylesheets/menu-footer.css', '/stylesheets/eventos.css']
-    })
-});
+router.get('/eventos', eventos);
 
 /* pagina Parceiros*/
-router.get('/parceiros', function(req, res, next) {
-  res.render('parceiros', {
-    css: ['/stylesheets/parceiros.css', '/stylesheets/menu-footer.css']
-  });
-});
+router.get('/parceiros', parceiros);
 
 /* pagina login*/
-router.get('/login', function(req, res, next) {
-  res.render('login', {
-    css: ['/stylesheets/menu-footer.css', '/stylesheets/login.css']
-  });
-});
+router.get('/login', login);
+
+/* pagina carrinho*/
+router.get('/carrinho', carrinho);
 
 /* pagina painel de usuário*/
 router.get('/painel-usuario', (req, res, next) => {
   res.render('painel-usuario', {
     css: ['/stylesheets/menu-footer.css', '/stylesheets/login.css']
-  });
-});
-
-/* pagina produto interno*/
-router.get('/produto-interno', function(req, res, next) {
-  res.render('prod-interno', {
-    css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
-  });
-});
-
-router.get('/produto-interno/:id', function (req, res, next) {
-  const { id } = req.params;
-  var produto = produtos.filter((prod) => prod.id == id);
-  produto = produto[0]
-  var arrayImg = produto.imagem;
-  var img = arrayImg[0]
-  console.log(produto)
-  produto.imagem = img
-  res.render('prod-interno', {produto,
-    css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
-  });
-});
-
-/* pagina carrinho*/
-router.get('/carrinho', (req, res) => {
-  res.render('carrinho', {
-    css: ['/stylesheets/menu-footer.css','/stylesheets/carrinho.css']
   });
 });
 
