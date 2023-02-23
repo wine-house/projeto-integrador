0e760d77b21540d7084dd352a210ef3825900d25
computador-matheus <matheusfotografia987@gmail.com>
2023-02-23 15:48:08 -0300

    #/organiza-rotas-de-produtos

--- src/controller/produtosController.js
+++ src/controller/produtosController.js
@@ -1,11 +1,11 @@
-const produtosModel = require('../models/produtosModel');
+const { Produto } = require('../../models');
 const produtoDataBase = require("../database/produtos.json");
 
 
 module.exports = {
-    index:  (req, res) => {
+    index: async (req, res) => {
         // controller comunicando com o model
-        const produtos = produtosModel.index();
+        const produtos = await Produto.findAll();
         // controller comunicando com a view
         return res.render('produtos', {
             produtos: produtos,
@@ -34,7 +34,19 @@ module.exports = {
             vinhoTipo,
             css:["/stylesheets/produtos.css","/stylesheets/menu-footer.css"]
         });
-       
-    
-    }
+    },
+    getProductById: async(req, res, next) => {
+        const { id } = req.params;
+        const produto = Produto.findByPk(id);
+        // var produto = produtos.filter((prod) => prod.id == id);
+        // produto = produto[0]
+        // var arrayImg = produto.imagem;
+        // var img = arrayImg[0]
+        // console.log(produto)
+        // produto.imagem = img
+
+        res.render('prod-interno', { produto: produto,
+          css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
+        });
+      }
 }
--- src/routes/produto.js
+++ src/routes/produto.js
@@ -6,7 +6,16 @@ const produtosController = require('../controller/produtosController');
 router.get('/', produtosController.index);
 
 /*pagina tipo de produtos*/
-router.get('/produtos/:tipo', produtosController.show);
-router.get('/produtos/teste/:id', produtosController.mostrarporId);
+router.get('/:tipo', produtosController.show);
+router.get('/teste/:id', produtosController.mostrarporId);
+
+/* pagina produto interno*/
+router.get('/produto-interno', function(req, res, next) {
+  res.render('prod-interno', {
+    css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
+  });
+});
+
+router.get('/produto-interno/:id', produtosController.getProductById);
 
 module.exports = router;
