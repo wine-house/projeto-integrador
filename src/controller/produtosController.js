672e2df07e5416a83bb8627e8c69a7a580f0f4b0
computador-matheus <matheusfotografia987@gmail.com>
2023-03-09 21:17:27 -0300

    feat:add-filter-from-categories

--- src/controller/produtosController.js
+++ src/controller/produtosController.js
@@ -1,56 +1,47 @@
 const { Produto } = require('../models');
-const produtoDataBase = require("../database/produtos.json");
+const { Sequelize, Op } = require('sequelize');
+
 
 
 module.exports = {
     index: async (req, res) => {
-        // controller comunicando com o model
-        const produtos = await Produto.findAll();
-        // controller comunicando com a view
-        return res.render('produtos', {  produtos,
-            css: ["/stylesheets/produtos.css","/stylesheets/menu-footer.css"]
-        });
-    },
+        try
+        {
+            const { categoria } = req.query;
+            // controller comunicando com o model
+            const produtos = await Produto.findAll({
+                where: categoria ? {
+                    categoria: {
+                        [Op.like]: `${categoria}`
+                    }
+                } : null
+            });
+            // controller comunicando com a view
 
+            const categorias = await Produto.findAll({
+                attributes: [[
+                    Sequelize.fn('DISTINCT', Sequelize.col('categoria')), 'categoria'
+                ]]
+            });
 
-    show: (req, res) => {
-        const { categoria } = req.params;
-        const vinhoCategoria = produtoDataBase.filter((prod) => prod.categoria == categoria)
-
-        return res.render('produto-listar', {
-            vinhoCategoria,
-            css:["/stylesheets/produtos.css","/stylesheets/menu-footer.css"]
-        });
-    },
-
-     mostrarporId: (req, res) => {
-        const { id } = req.params;
-        var vinhoCategoria = produtoDataBase.filter((prod) => prod.id == id)
-        console.log(vinhoCategoria)
-
-        return res.render('produto-listar', {
-
-            vinhoCategoria,
-            css:["/stylesheets/produtos.css","/stylesheets/menu-footer.css"]
-        });
+            return res.render('produtos', {  produtos, categorias,
+                css: ["/stylesheets/produtos.css","/stylesheets/menu-footer.css"]
+            });
+        } catch (err) {
+            console.log(err);
+        }
     },
     getProductById: async(req, res, next) => {
-        const { id } = req.params;
-        const produto = await Produto.findByPk(id);
-        // var produto = produtos.filter((prod) => prod.id == id);
-        // produto = produto[0]
-        // var arrayImg = produto.imagem;
-        // var img = arrayImg[0]
-        // console.log(produto)
-        // produto.imagem = img
-
-        res.render('prod-interno', { produto: produto,
-          css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
-        });
-      },
-      deletar: async(req, res) => {
-        const { id } = req.params;
-        await Produto.destroy(id);
-        
+        try
+        {
+            const { id } = req.params;
+            const produto = await Produto.findByPk(id);
+    
+            res.render('prod-interno', { produto: produto,
+                css: ['/stylesheets/menu-footer.css', '/stylesheets/prod-interno.css']
+            });
+        } catch (err){
+            console.log(err);
+        }
       }
 }
